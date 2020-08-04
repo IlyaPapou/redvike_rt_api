099a5ed5250addcd38a88da9147398bec1e73945
Ilya Papou <ilya.popov.sbmt@gmail.com>
2020-08-05 00:00:54 +0300

    Bookings service refactoring

--- app/booking/service.js
+++ app/booking/service.js
@@ -6,36 +6,43 @@ const delimiter = {
 	delimiter: ';',
 };
 
+const isIdsEqual = (id1, id2) => id1 === id2;
+
+const dataReader = (dataPath, onData, onEnd, onError) =>
+	fs
+		.createReadStream(path.join(__dirname, dataPath))
+		.pipe(csv.parse(delimiter))
+		.on('data', onData)
+		.on('end', onEnd)
+		.on('error', onError);
+
+const readAmentityData = (onData, onEnd, onError) =>
+	dataReader('./../../data/Amenity.csv', onData, onEnd, onError);
+
+const readReservationsData = (onData, onEnd, onError) =>
+	dataReader('./../../data/Reservations.csv', onData, onEnd, onError);
+
 const getAmenityById = (amenityId) =>
 	new Promise((resolve, reject) => {
 		const results = [];
-		const id = parseInt(amenityId);
-		fs.createReadStream(path.join(__dirname, './../../data/Amenity.csv'))
-			.pipe(csv.parse(delimiter))
-			.on('data', (data) => {
-				if (id === parseInt(data[0])) results.push(data);
-			})
-			.on('end', () => resolve(results))
-			.on('error', () => reject(new Error("Can't get Reservations by date")));
+		readAmentityData(
+			(data) => isIdsEqual(amenityId, data[0]) && results.push(data),
+			() => resolve(results),
+			() => reject(new Error("Can't get Amenity by id"))
+		);
 	});
 
 const getReservationsByAmenityIdAndDate = (amenityId, date) =>
 	new Promise((resolve, reject) => {
 		const results = [];
-		const id = parseInt(amenityId);
-		fs.createReadStream(path.join(__dirname, './../../data/Reservations.csv'))
-			.pipe(csv.parse(delimiter))
-			.on('data', (data) => {
-				if (
-					id === parseInt(data[1]) &&
-					isSameDay(parseInt(data[5]), parseInt(date))
-				)
-					results.push(data);
-			})
-			.on('end', () => resolve(results))
-			.on('error', () => {
-				reject(new Error("Can't get Reservations by date"));
-			});
+		readReservationsData(
+			(data) =>
+				isIdsEqual(amenityId, data[1]) &&
+				isSameDay(parseInt(data[5]), parseInt(date)) &&
+				results.push(data),
+			() => resolve(results),
+			() => reject(new Error("Can't get Reservations by date"))
+		);
 	});
 
 module.exports = {
