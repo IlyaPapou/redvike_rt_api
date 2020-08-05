88f2acb80d9a03f5ab603da85e5e7ef4d9de0b97
Ilya Papou <ilya.popov.sbmt@gmail.com>
2020-08-05 13:35:10 +0300

    Date transformer implemented

--- app/booking/transformer.service.js
+++ app/booking/transformer.service.js
@@ -2,10 +2,17 @@ const transformToBookingsModel = (reservations, amenity) =>
 	reservations.map((item) => ({
 		reservation_id: item[0],
 		user_id: item[2],
-		start_time: item[3], // TODO: start time in HH: MM format
+		start_time: getHoursAndMinutes(item[3], item[5]),
 		duration: item[4] - item[3],
 		amenity_name: amenity[0][1],
 	}));
+
+const getHoursAndMinutes = (totalMinutes, date) => {
+	const newDate = new Date(parseInt(date));
+	newDate.setMinutes(parseInt(totalMinutes));
+	const isoDate = newDate.toISOString();
+	return `${isoDate.substr(11, 5)}`;
+};
 
 module.exports = {
 	transformToBookingsModel,
